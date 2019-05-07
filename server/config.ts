ef3c3508659909bf51d26e189d2879317d46f8df
Matt Davidson <matt@mattdavidson.kiwi>
2019-05-07 14:28:46 +1000

    put config interface inside config file

--- server/config.ts
+++ server/config.ts
@@ -1,4 +1,39 @@
-import { IWakaConfig } from './types'
+import 'dotenv/config'
+
+interface IWakaConfig {
+  prefix:
+    | 'au-syd'
+    | 'au-cbr'
+    | 'au-seq'
+    | 'ch-sfr'
+    | 'nz-akl'
+    | 'nz-chc'
+    | 'nz-otg'
+    | 'nz-wlg'
+  version: string
+  mode: 'all' | 'db' | 'shapes' | 'unzip' | 'download' | 'export'
+  storageService: 'aws' | 'azure'
+  shapesContainer: string
+  shapesRegion: string
+  shapesSkip: boolean
+  emulatedStorage: boolean
+  local?: boolean
+  keyValue?: 'dynamo'
+  keyValueVersionTable?: string
+  keyValueRegion?: string
+  tfnswApiKey?: string
+  db: {
+    user: string
+    password: string
+    server: string
+    database: string
+    master_database: string
+    transactionLimit: number
+    connectionTimeout: number
+    requestTimeout: number
+  }
+  [key: string]: string | undefined | boolean | number | object
+}
 
 declare var process: {
   env: {
