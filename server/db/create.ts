5f06ea700ce116fc40653bb1e81d5c8f214d58c1
Matt Davidson <matt.davidson@axessadvisory.com>
2019-05-08 11:03:18 +1000

    move table creation to sep functions

--- server/db/create.ts
+++ server/db/create.ts
@@ -2,127 +2,177 @@ import { readFileSync } from 'fs'
 import { resolve } from 'path'
 import log from '../logger'
 import connection from './connection'
+const createAgencyTable = async () => {
+  return connection
+    .get()
+    .request()
+    .batch(readFileSync(resolve(__dirname, './procs/agency.sql')).toString())
+}
+
+const createStopsTable = async () => {
+  return connection
+    .get()
+    .request()
+    .batch(readFileSync(resolve(__dirname, './procs/stops.sql')).toString())
+}
+
+const createRoutesTable = async () => {
+  return connection
+    .get()
+    .request()
+    .batch(readFileSync(resolve(__dirname, './procs/routes.sql')).toString())
+}
+
+const createTripsTables = async () => {
+  return connection
+    .get()
+    .request()
+    .batch(readFileSync(resolve(__dirname, './procs/trips.sql')).toString())
+}
+
+const createStopTimesTable = async () => {
+  return connection
+    .get()
+    .request()
+    .batch(
+      readFileSync(resolve(__dirname, './procs/stop_times.sql')).toString()
+    )
+}
+const createCalendarTable = async () => {
+  return connection
+    .get()
+    .request()
+    .batch(readFileSync(resolve(__dirname, './procs/calendar.sql')).toString())
+}
+
+const createCalendarDatesTable = async () => {
+  return connection
+    .get()
+    .request()
+    .batch(
+      readFileSync(resolve(__dirname, './procs/calendar_dates.sql')).toString()
+    )
+}
+const createTempCalendarTable = async () => {
+  return connection
+    .get()
+    .request()
+    .batch(
+      readFileSync(resolve(__dirname, './procs/temp_calendar.sql')).toString()
+    )
+}
+const createTempStopTimesTable = async () => {
+  return connection
+    .get()
+    .request()
+    .batch(
+      readFileSync(resolve(__dirname, './procs/temp_stop_times.sql')).toString()
+    )
+}
+
+const createTempCalendarDates = async () => {
+  return connection
+    .get()
+    .request()
+    .batch(
+      readFileSync(
+        resolve(__dirname, './procs/temp_calendar_dates.sql')
+      ).toString()
+    )
+}
+
+const createTempAgencyTable = async () => {
+  return connection
+    .get()
+    .request()
+    .batch(
+      readFileSync(resolve(__dirname, './procs/temp_agency.sql')).toString()
+    )
+}
+const createTempStopsTable = async () => {
+  return connection
+    .get()
+    .request()
+    .batch(
+      readFileSync(resolve(__dirname, './procs/temp_stops.sql')).toString()
+    )
+}
+const createTempRoutesTable = async () => {
+  return connection
+    .get()
+    .request()
+    .batch(
+      readFileSync(resolve(__dirname, './procs/temp_routes.sql')).toString()
+    )
+}
+const createTempTripsTable = async () => {
+  return connection
+    .get()
+    .request()
+    .batch(
+      readFileSync(resolve(__dirname, './procs/temp_trips.sql')).toString()
+    )
+}
+
+const createStopTimesStoredProc = async () => {
+  return connection
+    .get()
+    .request()
+    .batch(
+      readFileSync(resolve(__dirname, './procs/GetStopTimes.sql')).toString()
+    )
+}
+const createMultipleStopTimesStoredProc = async () => {
+  return connection
+    .get()
+    .request()
+    .batch(
+      readFileSync(
+        resolve(__dirname, './procs/GetMultipleStopTimes.sql')
+      ).toString()
+    )
+}
+const createTimetableStoredProc = async () => {
+  return connection
+    .get()
+    .request()
+    .batch(
+      readFileSync(resolve(__dirname, './procs/GetTimetable.sql')).toString()
+    )
+}
+const createMultipleTimetableStoredProc = async () => {
+  return connection
+    .get()
+    .request()
+    .batch(
+      readFileSync(
+        resolve(__dirname, './procs/GetMultipleTimetable.sql')
+      ).toString()
+    )
+}
 
 const create = async () => {
   log('Creating Tables...')
-  try {
-    await connection
-      .get()
-      .request()
-      .batch(readFileSync(resolve(__dirname, './procs/agency.sql')).toString())
-    await connection
-      .get()
-      .request()
-      .batch(readFileSync(resolve(__dirname, './procs/stops.sql')).toString())
-    await connection
-      .get()
-      .request()
-      .batch(readFileSync(resolve(__dirname, './procs/routes.sql')).toString())
-    await connection
-      .get()
-      .request()
-      .batch(readFileSync(resolve(__dirname, './procs/trips.sql')).toString())
-    await connection
-      .get()
-      .request()
-      .batch(
-        readFileSync(resolve(__dirname, './procs/stop_times.sql')).toString()
-      )
-    await connection
-      .get()
-      .request()
-      .batch(
-        readFileSync(resolve(__dirname, './procs/calendar.sql')).toString()
-      )
-    await connection
-      .get()
-      .request()
-      .batch(
-        readFileSync(
-          resolve(__dirname, './procs/calendar_dates.sql')
-        ).toString()
-      )
-    await connection
-      .get()
-      .request()
-      .batch(
-        readFileSync(resolve(__dirname, './procs/temp_agency.sql')).toString()
-      )
-    await connection
-      .get()
-      .request()
-      .batch(
-        readFileSync(resolve(__dirname, './procs/temp_stops.sql')).toString()
-      )
-    await connection
-      .get()
-      .request()
-      .batch(
-        readFileSync(resolve(__dirname, './procs/temp_routes.sql')).toString()
-      )
-    await connection
-      .get()
-      .request()
-      .batch(
-        readFileSync(resolve(__dirname, './procs/temp_trips.sql')).toString()
-      )
-    await connection
-      .get()
-      .request()
-      .batch(
-        readFileSync(
-          resolve(__dirname, './procs/temp_stop_times.sql')
-        ).toString()
-      )
-    await connection
-      .get()
-      .request()
-      .batch(
-        readFileSync(resolve(__dirname, './procs/temp_calendar.sql')).toString()
-      )
-    await connection
-      .get()
-      .request()
-      .batch(
-        readFileSync(
-          resolve(__dirname, './procs/temp_calendar_dates.sql')
-        ).toString()
-      )
-
-    log('Creating Stored Procedures...')
-    await connection
-      .get()
-      .request()
-      .batch(
-        readFileSync(resolve(__dirname, './procs/GetStopTimes.sql')).toString()
-      )
-    await connection
-      .get()
-      .request()
-      .batch(
-        readFileSync(
-          resolve(__dirname, './procs/GetMultipleStopTimes.sql')
-        ).toString()
-      )
-    await connection
-      .get()
-      .request()
-      .batch(
-        readFileSync(resolve(__dirname, './procs/GetTimetable.sql')).toString()
-      )
-    await connection
-      .get()
-      .request()
-      .batch(
-        readFileSync(
-          resolve(__dirname, './procs/GetMultipleTimetable.sql')
-        ).toString()
-      )
-
-    log('Database Created')
-  } catch (error) {
-    console.error(error)
-  }
+  await createAgencyTable()
+  await createStopsTable()
+  await createRoutesTable()
+  await createTripsTables()
+  await createStopTimesTable()
+  await createCalendarTable()
+  await createCalendarDatesTable()
+  await createTempStopTimesTable()
+  await createTempAgencyTable()
+  await createTempStopsTable()
+  await createTempRoutesTable()
+  await createTempTripsTable()
+  await createTempCalendarTable()
+  await createTempCalendarDates()
+  log('Creating Stored Procedures...')
+  await createStopTimesStoredProc()
+  await createMultipleStopTimesStoredProc()
+  await createTimetableStoredProc()
+  await createMultipleTimetableStoredProc()
+  log('Database Created')
 }
 
 class CreateDB {
